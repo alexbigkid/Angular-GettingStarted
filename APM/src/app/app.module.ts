1731a3deff5478d795ea8e70e93f4584e8f45764
Alex Berger <alexbigkid@users.noreply.github.com>
2020-11-21 02:02:25 -0800

    F learning: added custom pipe

--- APM/src/app/app.module.ts
+++ APM/src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
-import { ProductListComponent } from './products/product-list.component'
 import { FormsModule } from '@angular/forms';
 
+import { AppComponent } from './app.component';
+import { ProductListComponent } from './products/product-list.component';
+import { ConvertToSpacesPipe } from './shared/converrt-to-spaces.pipe';
+
 @NgModule({
-    declarations: [ AppComponent, ProductListComponent ],
+    declarations: [ AppComponent, ProductListComponent, ConvertToSpacesPipe ],
     imports: [ BrowserModule, FormsModule ],
     bootstrap: [AppComponent]
 })
