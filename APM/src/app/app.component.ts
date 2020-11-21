829e7c8b5be570dedced9f333d3ef5be73a1013f
Alex Berger <alexbigkid@users.noreply.github.com>
2020-11-20 22:52:15 -0800

    F added ProductListComponent

--- APM/src/app/app.component.ts
+++ APM/src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+// import { ProductListComponent } from './products/product-list.component'
 
+
+// @Component({
+//   selector: 'pm-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css']
+// })
 @Component({
-  selector: 'pm-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'pm-root',
+    template: `
+        <div>
+            <h1>{{pageTitle}}</h1>
+            <pm-products></pm-products>
+        </div>
+`
 })
+
 export class AppComponent {
-  title = 'Angular: Getting Started';
+    pageTitle: string = 'Angular: Getting Started';
 }
