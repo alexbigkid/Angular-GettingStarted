19fcdcaf86c6279138b02ea00d47d0d4f1f6fe50
Alex Berger <alexbigkid@users.noreply.github.com>
2021-02-04 16:41:28 -0800

    F - implemented StarComponent with Input()

--- APM/src/app/app.module.ts
+++ APM/src/app/app.module.ts
@@ -5,10 +5,11 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
 import { ConvertToSpacesPipe } from './shared/converrt-to-spaces.pipe';
+import { StarComponent } from './shared/star.component';
 
 @NgModule({
-    declarations: [ AppComponent, ProductListComponent, ConvertToSpacesPipe ],
     imports: [ BrowserModule, FormsModule ],
+    declarations: [ AppComponent, ProductListComponent, ConvertToSpacesPipe, StarComponent ],
     bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ APM/src/app/shared/star.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+
+@Component({
+    selector: 'pm-star',
+    templateUrl: './star.component.html',
+    styleUrls: ['./star.component.css']
+})
+
+export class StarComponent  implements OnChanges {
+    @Input() rating: number;
+    starWidth: number;
+
+    ngOnChanges(changes: SimpleChanges): void {
+        this.starWidth = this.rating * 75 / 5;
+    }
+}
